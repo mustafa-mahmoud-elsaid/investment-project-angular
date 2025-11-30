f8f95420ec8df92939394748ee3e5bb6f9b43ac1
Mostafa Mahmoud <mostafapro87@gmail.com>
2025-11-30 15:09:30 +0200

    2 way binding implement in user-input-form

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+interface InvestmentModel {
+  initialInvestment: number;
+  annualInvestment: number;
+  expectedInvestment: number;
+  duration: number;
+}
 
 @Component({
   selector: 'app-user-input',
@@ -9,8 +15,17 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  enteredName: string = '';
-  enteredId: string = '';
-  enteredAmount = 0;
-  onSubmit() {}
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '0';
+  @Output() submit = new EventEmitter<InvestmentModel>();
+  onSubmit() {
+    this.submit.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      annualInvestment: +this.enteredAnnualInvestment,
+      expectedInvestment: +this.enteredExpectedReturn,
+      duration: +this.enteredDuration,
+    });
+  }
 }
